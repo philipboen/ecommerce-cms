fdcf30fbb48c587724da07f81b6020f3d959871c
Philip Boen <philipboen001@gmail.com>
2024-06-02 12:00:41 +0300

    remove hydration error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
-import { UserButton } from "@clerk/nextjs";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { db } from "@/lib/db";
@@ -12,7 +12,7 @@ export const Navbar = async () => {
     redirect("/sign-in");
   }
 
-  const store = await db.store.findMany({
+  const stores = await db.store.findMany({
     where: {
       userId,
     },
@@ -21,10 +21,13 @@ export const Navbar = async () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <StoreSwitcher items={store} />
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
+
         <div className="ml-auto flex items-center space-x-4">
-          <UserButton />
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       </div>
     </div>
