f2883f418caabc915bcbc4cb830a0b850c18ddd2
Philip Boen <philipboen001@gmail.com>
2024-07-09 16:23:58 +0300

    feat: complete the Dashboard(admin) page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,21 +1,73 @@
-import { db } from "@/lib/db";
+import { CreditCard, DollarSign, Package } from "lucide-react";
+import { priceFormatter } from "@/lib/utils";
+import { getTotalRevenue } from "@/lib/actions/get-total-revenue";
+import { Heading } from "@/components/shared/heading";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { getSalesCount } from "@/lib/actions/get-sales-count";
+import { getStockCount } from "@/lib/actions/get-stock-count";
+import { Overview } from "@/components/overview";
+import { getGraphRevenue } from "@/lib/actions/get-graph-revenue";
 
 interface DashboardPageProps {
   params: { storeId: string };
 }
 
 const DashboardPage = async ({ params }: DashboardPageProps) => {
-  const store = await db.store.findFirst({
-    where: {
-      id: params.storeId,
-    },
-  });
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
+  const graphRevenue = await getGraphRevenue(params.storeId);
 
   return (
-    <div className="space-y-2 p-4">
-      <p>Store ID: {store?.id}</p>
-      <p>Store Name: {store?.name}</p>
-      <p>This is the DashboardPage</p>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid grid-cols-3 gap-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="size-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {priceFormatter.format(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-2">
+              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CreditCard className="size-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">+{salesCount}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-2">
+              <CardTitle className="text-sm font-medium">
+                Products In Stock
+              </CardTitle>
+              <Package className="size-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{stockCount}</div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 };
